ed3fb30cc808cf1fc6dfe5c21c9d371ce194594d
Christian Covington <christian.t.covington@gmail.com>
2019-08-01 15:24:08 -0400

    noise separate

--- snapping_mechanism/utility_testing/cc_laplace.py
+++ snapping_mechanism/utility_testing/cc_laplace.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def add_laplace_noise(non_private_estimate, sensitivity, epsilon):
+def laplace_noise(sensitivity, epsilon):
     _lambda = sensitivity / epsilon
-    private_estimate = non_private_estimate + np.random.laplace(loc = 0, scale = _lambda)
-    return(private_estimate)
+    noise = np.random.laplace(loc = 0, scale = _lambda)
+    return(noise)
