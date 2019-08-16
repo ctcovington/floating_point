fix location of sign

snapping_mechanism/implementation/cc_snap.py
@@ -214,8 +214,7 @@ class Snapping_Mechanism:
         Return:
             numeric: sample from Unif(0,1)
         """
-        sign = gmpy2.mpfr(secure_random.sample(population = [-1,1], k = 1)[0]) # using mpfr precision here, even though it's an integer, so that the
-                                                                               # precision carries through in later steps
+
         '''
         Looking for more elegant way to sample from geometric
         '''
@@ -223,7 +222,6 @@ class Snapping_Mechanism:
         while (secure_random.sample(population = [0,1], k = 1)[0] == 0):
             geom += 1
         u_star_exponent = bin(-geom + 1023)[2:]
-        # u_star_exponent = bin(-np.random.geometric(p = 0.5) + 1023)[2:]
         u_star_mantissa = ''.join([str(secure_random.sample(population = [0,1], k = 1)[0]) for i in range(52)])
         u_star_sample = self._bin_to_double('0' + str(u_star_exponent) + str(u_star_mantissa))
         return(u_star_sample)
@@ -284,6 +282,8 @@ class Snapping_Mechanism:
         '''
         # instantiate instance of cryptographically secure random class and generate random sign and draw from Unif(0,1)
         secure_random = random.SystemRandom()
+        sign = gmpy2.mpfr(secure_random.sample(population = [-1,1], k = 1)[0]) # using mpfr precision here, even though it's an integer, so that the
+                                                                               # precision carries through in later steps
         u_star_sample = self._sample_from_uniform(secure_random)
         epsilon_prime = self._redefine_epsilon(self.epsilon, B_scaled, precision)
         inner_result = self._clamp(mechanism_input_scaled, B_scaled) + (sign * 1/epsilon_prime * crlibm.log_rn(u_star_sample))